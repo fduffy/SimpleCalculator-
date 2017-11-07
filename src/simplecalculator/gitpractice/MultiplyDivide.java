794f0ff314cbbe7937cf055ff920b30419678669
wildfrancis <fduffy@student.bridgew.edu>
2017-11-07 14:07:00 -0500

    added feature branch MultiplyDivide

---
+++ src/simplecalculator/gitpractice/MultiplyDivide.java
@@ -0,0 +1,16 @@
+package simplecalculator.gitpractice;
+
+public class MultiplyDivide {
+
+	public static float multiply(float a, float b)
+	{
+		float result = a * b;
+		return result;
+	}
+	
+	public static float divide(float a, float b)
+	{
+		float result = a / b;
+		return result;
+	}
+}

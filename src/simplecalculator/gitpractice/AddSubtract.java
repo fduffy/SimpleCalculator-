f650a3ce5c3cfef00ee1470cd0fa97a63a1d0bbf
Slav Bugryn <>
2017-11-07 19:47:26 -0500

    Added 'AddSubtract' feature

---
+++ src/simplecalculator/gitpractice/AddSubtract.java
@@ -0,0 +1,15 @@
+package simplecalculator.gitpractice;
+
+public class AddSubtract {
+	
+	public static float add( float a, float b) {
+		float result = a+b;
+		return result;
+	}
+	
+	public static float subtract( float a, float b) {
+		float result = a-b;
+		return result;
+	}
+	
+}
